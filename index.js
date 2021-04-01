178949ba118c7eeb99517f36f015e422d4a7f122
cvaudrain <cvaudrain@gmail.com>
2021-04-01 18:02:10 -0400

    complete all except when start button is pushed more than once, consecutively or not, break button doesn't work properly.

--- index.js
+++ index.js
@@ -1,39 +1,72 @@
 var minutes = 25;
 var seconds = 0;
 var paused = false;
-var running = false; /* use this var to toggle when timer is running or not. This can switch on/off the
-start button click event, so that you can't mash the button and run the countdown function over and over to break it.  */
-//pause button function
+var running = false;
+var btnRead = "break"
+var startClicked = false;
+
+$('#start').data('clicked', false)
+
+
 function pauseToggle(){
+  // if(running == false && paused == false){
+  //   return;
+  // }
   if (paused == false && running == true){
 running = false;
   paused = true;
   $('#pause').html("Resume");
-} else if(running == false && paused == true){
+} else if(running == false ){
   paused = false;
   running = true;
   $('#pause').html("Pause");
 }
 
 }
+//break/work toggle button BEFORE timer start has been clicked
+$('#break').click(function(){
+if(startClicked == true){
+  return;
+  console.log("Clicked is TRUE 1st break version");
+}
+// else {
+//
+// }
+  if(btnRead == "break"){
+  minutes = 5;
+  $('#minutes').html(minutes)
+  seconds = 0;
+  $('#seconds').html(seconds+"0")
+  running = false;
+  paused = false;
+  $('#pause').html("Pause")
+  btnRead = "work"
+    $('#break').html("Work")
+} else{
+  console.log("btnRead not set to break")
+  minutes = 25;
+  $('#minutes').html(minutes)
+  seconds = 0;
+  $('#seconds').html(seconds+"0")
+  running = false;
+  paused = false;
+  $('#pause').html("Pause")
+  btnRead = "break"
+    $('#break').html("Break")
+}
 
-//Break Button function
-
-//Reset Button function
-
-
-
+})
 //Event Listeners to launch each function
 
 
 $('#pause').click(function(){
-
   pauseToggle()
 })
 
 //start and reset must be scoped into tyhe same locality i.e this function, in order to allow clearInterval to reference let countdown. countdown is now bound to a let for this reason.
 $("#start").on("click",function(){
-if(/*paused == false && */ running == false){
+  startClicked = true;
+if(paused == false &&  running == false){
   running = true;
   let countdown = setInterval(function(){
   if(paused == false && minutes + seconds > 0){
@@ -54,11 +87,17 @@ if(/*paused == false && */ running == false){
   clearInterval(countdown)
   }
   console.log("running")
-  },10)
+},1000)
+
 
   $('#reset').click(function(){ //This event listener only works after start has been clicked. It's fine bc you wouldn't need this button if the clock was already at 25:00.
-      clearInterval(countdown)
+     function reset(){
+        clearInterval(countdown);
+      if(btnRead == "break"){ //bc btn will read break if timer is currently work mode. reset() should reset timer for either one, depending.
   minutes = 25;
+}else{
+  minutes = 5;
+}
   $('#minutes').html(minutes)
   seconds = 0;
   $('#seconds').html(seconds+"0")
@@ -67,8 +106,64 @@ if(/*paused == false && */ running == false){
   $('#pause').html("Pause")
 
 
-
-  })
 }
+reset();
+//break/work toggle button
+//
+
+})
+$('#break').click(function(){
+  console.log("second break function ran")
+  clearInterval(countdown); //stops timer running
+//Next comes reset function:
+// clearInterval(countdown);
+// if(btnRead == "break"){ //bc btn will read break if timer is currently work mode. reset() should reset timer for either one, depending.
+// minutes = 5;
+// }else{
+// minutes = 25;
+// }
+$('#minutes').html(minutes)
+seconds = 0;
+$('#seconds').html(seconds+"0")
+running = false;
+paused = false;
+$('#pause').html("Pause")
+
+  if(btnRead == "break"){
+  minutes = 5;
+  $('#minutes').html(minutes)
+  seconds = 0;
+  $('#seconds').html(seconds+"0")
+
+  $('#pause').html("Pause")
+  btnRead = "work"
+    $('#break').html("Work")
+    running = false;
+    paused = false;
+} else{
+  minutes = 25;
+  $('#minutes').html(minutes)
+  seconds = 0;
+  $('#seconds').html(seconds+"0")
+
+  $('#pause').html("Pause")
+  btnRead = "break"
+    $('#break').html("Break")
+    running = false;
+    paused = false;
+}
+
+})
+  // })
+//
+}
+
+})
+
+
+$(document).keydown(function(){
+  console.log("Puased = "+paused)
+  console.log("Running = "+running)
+  console.log("btnRead = "+btnRead)
 
 })
