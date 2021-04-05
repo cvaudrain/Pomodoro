6561599f75fa9f385d0b4ceb367d033d9a10e93e
cvaudrain <cvaudrain@gmail.com>
2021-04-04 22:03:11 -0400

    Complete app. Functional and bug free. Only need to delete unwanted comments, clone repository to main, and host on github.

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ Owner: Chris Vaudrain
 */
 //Global Variables
 var chime = new Audio("sounds/jazzhop.m4a")
-var minutes = 24;
-var seconds = 60;
+var minutes = 1;
+var seconds = 0;
 var minPassed = 0;
-// var secPassed = 0;
-var idVar = setInterval(count,1000) //this runs the interval on count. needs to be global so that clear(idVar) can be called from anywhere
+var idVar = setInterval(count,200) //this runs the interval on count. needs to be global so that clear(idVar) can be called from anywhere
 function onOffSwitch(){
   clearInterval(idVar) //will be called to halt idVar.
 }
@@ -20,38 +19,37 @@ var status = "off";
 
 function count(){ //this increments and sets the clock accordingly.
   console.log(minutes+":"+seconds)
+  if(seconds==0){
+    minutes -=1;
+    minPassed+=1;
+    seconds = 60; //will be decremented prior to setting the HTML onscreen, so it will go from :00 to :59 smoothly.
+    $("#Minutes").html(minutes);
+  }
 seconds-=1;
-// secPassed+=1;
+$("Minutes").html(minutes);
 $("#Seconds").html(seconds);
-if(minutes==24){
-  $("#Minutes").html("24") // PROBABLY BUG LOOK BUG BUG BUG BUG BUG OR MAYBE NOT !!!
-}else{
-  $("#Minutes").html("4")
-}
 if(seconds <10){
   $("#Seconds").html("0"+seconds);
 }
-if(seconds==0){
-  minutes -=1;
-  minPassed+=1;
-  seconds = 60; //will be decremented prior to setting the HTML onscreen, so it will go from :00 to :59 smoothly.
-  $("#Minutes").html(minutes);
-}
 if(minutes+seconds == 0){
-  play(chime);
+  onOffSwitch();
+  chimeTime();
 }
-
-}
+                }
 
 function start(){
-//onOffSwitch = "on"; //this is the "on" switch. It redefines our clearInterval
+console.log(minutes+":"+seconds)
+if(minutes+seconds == 0){ return;}
 $("#Start-pause").html("Pause");
-idVar = setInterval(count,1000); //THIS LINE IS KEY LOOK HERE. we can't reference idVar as idVar(). We also can't ref as let idVar= setInterval. It HAS to redefinean existing....
+idVar = setInterval(count,20); //THIS LINE IS KEY LOOK HERE. we can't reference idVar as idVar(). We also can't ref as let idVar= setInterval. It HAS to redefinean existing....
 status="on"                      //..  let (i.e a variable). CANNOT create new Let, because the SCOPE is then BLOCK level and our pause() cannot reach it with onOffSwitch() anymore.
-console.log(status)               //Note to future self. Redefining at lock level is excellent. Re-DECLARING ruins scoping
+console.log(status)
+          //Note to future self. Redefining at lock level is excellent. Re-DECLARING ruins scoping
 }
 
 function pause(){
+  status="off"
+if(minutes+seconds==0){return;}
 $("#Start-pause").html("Start")
 onOffSwitch(); //calls clearInterval with proper scope.
 //onOffSwitch = clearInterval(idVar);
@@ -59,36 +57,59 @@ onOffSwitch(); //calls clearInterval with proper scope.
 console.log(idVar)
 status="off"
 console.log(status)
+
 }
 
 function reset(){
+  pause();
 minutes+=minPassed;
-seconds=60;
-$("#Minutes").html(minutes+1);
-// setTimeout(function(){  ///BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG BUG this shouldnt be here it should timeout then add 1 start when it starts running!!!!!!!!!!!!
-//   $("#Minutes").html(minutes);
-// },1000)
+seconds=0; //TRYING
+$("#Minutes").html(minutes);
 $("#Seconds").html("00");
 minPassed=0;
 secPassed=0;
-pause(chime);
+chime.pause();
+
+$("#Start-pause").html("Start")
+console.log("Reset: minutes ="+minutes)
 }
 
 function toggle(){
 reset();
 if(minutes>6){ //i.e if it's currently 25 after reset()
   minutes=5;
+  $("#Toggle").html("Work")
 }
 else{
   minutes = 25
+  $("#Toggle").html("Break")
 }
 $("#Minutes").html(minutes);
 $("#Seconds").html("00");
 }
 
-// function chime(){
-//
+function chimeTime(){ //I did this to practice chaining the short loop sound. Just as well to make the alarm file 3x as long lol
+  console.log(minutes+seconds)
+  chime.play();
+//    setTimeout(function(){
+//     console.log("timeout begun success")
+//   if(minutes+seconds==0){
+// chime.play();
 // }
+// setTimeout(function(){
+//  console.log("timeout 2 begun success")
+// if(minutes+seconds==0){
+// chime.play();
+// }
+// setTimeout(function(){
+//  console.log("timeout 3 begun success")
+// if(minutes+seconds==0){
+// chime.play();
+// }
+// },10600)
+// },10600)
+// },10600)
+}
 
 
 //
